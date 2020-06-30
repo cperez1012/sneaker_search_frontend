58cc8211e38926f6eb3a6c05f0848c39df875f2f
cperez1012 <perezchristian1012@gmail.com>
2020-06-30 18:17:37 -0400

    added patch request and working on fixing sneaker attributes being accessible for post request

--- src/index.js
+++ src/index.js
@@ -1,13 +1,24 @@
 const endPoint = "http://localhost:3000/api/v1/sneakers"
 
 document.addEventListener('DOMContentLoaded', () => {
-  // fetch and load syllabi
+  // fetch and load sneakers
   console.log("DOM is Loaded");
   getSneakers()
 
   const createSneakerForm = document.querySelector("#create-sneaker-form")
 
   createSneakerForm.addEventListener("submit", (e) => createFormHandler(e))
+
+  const sneakerContainer = document.querySelector('#sneaker-container')
+  // render edit form once its clicked
+  sneakerContainer.addEventListener("click", e => {
+    const id = parseInt(e.target.dataset.id);
+    const sneaker = Sneaker.findById(id);
+    // console.log(sneaker);
+    document.querySelector('#update-sneaker').innerHTML = sneaker.renderUpdateForm();
+    // This is the associated instance 
+  });
+  document.querySelector('#update-sneaker').addEventListener("click", e => updateFormHandler(e))
 })
 
 function getSneakers() {
@@ -37,18 +48,18 @@ function getSneakers() {
     })
 }
 
-function render(sneaker) {
-  const sneakerMarkup = `
-    <div data-id=${sneaker.id}>
-      <img src=${sneaker.attributes.image_url} height="200" width="250">
-      <h3>${sneaker.attributes.name}</h3>
-      <p>${sneaker.attributes.category.name}</p>
-      <button data-id=${sneaker.id}>edit</button>
-    </div>
-  <br><br>`;
+// function render(sneaker) {
+//   const sneakerMarkup = `
+//     <div data-id=${sneaker.id}>
+//       <img src=${sneaker.attributes.image_url} height="200" width="250">
+//       <h3>${sneaker.attributes.name}</h3>
+//       <p>${sneaker.attributes.category.name}</p>
+//       <button data-id=${sneaker.id}>edit</button>
+//     </div>
+//   <br><br>`;
 
-  document.querySelector('#sneaker-container').innerHTML += sneakerMarkup;
-}
+//   document.querySelector('#sneaker-container').innerHTML += sneakerMarkup;
+// }
 
 function createFormHandler(e) {
   e.preventDefault()
@@ -63,7 +74,7 @@ function postSneaker(name, description, image_url, category_id) {
   // confirm these values are coming through properly
   // build body object
   
-  let bodyData = {name, description, image_url, category_id}
+  const bodyData = {name, description, image_url, category_id}
 
   fetch(endPoint, {
     // POST request
@@ -73,7 +84,9 @@ function postSneaker(name, description, image_url, category_id) {
   })
   .then(response => response.json())
   .then(sneaker => {
+    console.log(sneaker)
     const sneakerData = sneaker.data
+
     // render JSON response
     // const sneakerMarkup = `
     // <div data-id=${sneaker.id}>
@@ -87,8 +100,36 @@ function postSneaker(name, description, image_url, category_id) {
     // document.querySelector('#sneaker-container').innerHTML += sneakerMarkup;
     // render(sneakerData)
     // Using the newSneaker defined variable in my post request and changing it to sneakerData
-    let newSneaker = new Sneaker(sneakerData, sneaker.attributes)
+    debugger
+    let newSneaker = new Sneaker(sneakerData, sneakerData.attributes)
+    // debugger
        
     document.querySelector('#sneaker-container').innerHTML += newSneaker.renderSneakerCard();  
   })
+
+  function updateFormHandler(e) {
+    e.preventDefault();
+    const id = parseInt(e.target.dataset.id)
+    const sneaker = Sneaker.findById(id)
+    const name = e.target.querySelector('#input-name').value
+    const description = e.target.querySelector('#input-description').value
+    const image_url = e.target.querySelector('#input-url').value
+    const category_id = parseInt(e.target.querySelector('#categories').value)
+    patchSneaker(sneaker, name, description, image_url, category_id)
+  }
+
+  function patchSneaker(sneaker, name, description, image_url, category_id) {
+    const bodyJSON = { name, description, image_url, category_id }
+    fetch(`http://localhost:3000/api/v1/sneakers/${sneaker.id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+      },
+      body: JSON.stringify(bodyJSON),
+    })
+      .then(response => response.json())
+      // our backend responds with the updated sneaker instance represented as JSON
+      .then(updatedNote => console.log(updatedNote))
+  }
 }
--- src/sneaker.js
+++ src/sneaker.js
@@ -16,6 +16,7 @@ class Sneaker {
         this.image_url = sneakerAttributes.image_url
         this.category = sneakerAttributes.category
         Sneaker.all.push(this)
+        console.log(this)
         // debugger
         // Push all instances of this to the array
     }
@@ -23,17 +24,67 @@ class Sneaker {
     renderSneakerCard() {
         // debugger
         // console.log(this)
-        return `
-          <div data-id=${this.id}>
-              <img src=${this.image_url} height="200" width="250">
-              <h3>${this.name}</h3>
-              <p>${this.category.name}</p>
-              <button data-id=${this.id}>edit</button>
+        // return `
+        //   <div data-id=${this.id}>
+        //       <img src=${this.image_url} height="200" width="250">
+        //       <h3>${this.name}</h3>
+        //       <p>${this.category.name}</p>
+        //       <button data-id=${this.id}>edit</button>
+        //   </div>
+        //   <br><br>`;
+          
+          return `
+          <div class="col-md-4">
+            <div class="card mb-4 shadow-sm">
+              <img src=${this.image_url} class="card-img-top" alt="...">
+              <div class="card-body">
+                <h5 class="card-title">${this.name}</h5>
+                <p class="card-text">${this.description}</p>
+                <div class="d-flex justify-content-between align-items-center">
+                  <div class="btn-group">
+                    <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+                    <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+                  </div>
+                  <small class="text-muted">Category: ${this.category.name}</small>
+                </div>
+              </div>
+            </div>
           </div>
-          <br><br>`;
+          `
     }
     static findById(id) {
         return this.all.find(sneaker => sneaker.id === id);
+    }
+
+    renderUpdateForm() {
+        return `
+        <form data-id=${this.id}>
+            <h3>Edit a Sneaker!</h3>
+            
+            <label>Sneaker Name</label>
+            <input id='input-name' type="text" name="name" value="${this.name}" class="input-text">
+            <br><br>
+
+            <label>Sneaker Description</label>
+            <text area id='input-description' name="description" rows="8"  cols="80" value="">${this.description}</textarea>
+            <br><br>
+
+            <label>Image URL</label>
+            <input id='input-url' type="text" name="image" value="${this.image_url}" class="input-text">
+            <br><br>
+
+            <label>Sneaker Category</label>
+            <select id='input-categories' type="text" name="categories" value="${this.category.name}">
+                <option value="1">Basketball</option>
+                <option value="2">Lifestyle</option>
+                <option value="3">Running</option>
+            </select>
+            <br><br>
+
+            <input id='edit button' type="submit" name="submit" value="Edit Sneaker" class="submit">
+        </form>
+
+        ` 
     }
 }
 
